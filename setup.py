e876fa4811ab494fff8d2957bb04aa4211cb1134
Adam Schubert <adam.schubert@sg1-game.net>
2021-05-27 23:39:48 +0200

    fix(Requirements): Pass requriements from requirements.txt to setup.py install_requires

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
+import os
+import sys
 from setuptools import setup, find_packages
 
+
+def get_requirements(filename: str) -> list:
+    return open(os.path.join(filename)).read().splitlines()
+
+install_requires = get_requirements('requirements.txt')
+if sys.version_info < (3, 0):
+    install_requires.append('futures')
 
 setup(
     name='xscreensaver-bouncing-text',
     version='1.0.5',
     packages=find_packages(exclude=['tests', 'tests.*']),
     package_data={'xscreensaver_bouncing_text': ['py.typed']},
-    install_requires=[],
+    install_requires=install_requires,
     url='https://github.com/Salamek/xscreensaver-bouncing-text',
     license='LGPL-3.0 ',
     author='Adam Schubert',
@@ -39,7 +48,7 @@ setup(
         'pylint',
         'tox',
         'pytest-cov'
-    ],
+    ].extend(install_requires),
     entry_points={
         'console_scripts': [
             'xscreensaver-bouncing-text = xscreensaver_bouncing_text.__main__:main',
